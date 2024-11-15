e4cd6ca2eed14735b0786cf40196f2b3207fe5ca
sandyvirat <sandyvirat123@gmail.com>
2024-11-15 15:34:18 +0100

    mongoDB commit

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
+from pymongo import MongoClient
+
+print("connecting to mongdo")
+client = MongoClient("mongodb://localhost:27017/myDatabase?retryWrites=true&w=majority")
+db = client['myDatabase']  # Replace with your database name
+collection = db['sandy']  # Replace with your collection name
+print("Mongo db connected sucessfully")
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')  # This will render your HTML form
+
+@app.route('/add', methods=['POST'])
+def add_document():
+    data = request.json
+    result = collection.insert_one(data)
+    return jsonify({"Inserted ID": str(result.inserted_id)})
 
 @app.route('/greet', methods=['POST'])
 def greet():
@@ -18,7 +31,14 @@ def greet():
     age_message = f"You are {age} years old."
     hobby_message = f"Your favorite hobby is {hobby}."
     mood_message = f"You're feeling {mood} today!"
-
+    try:
+        data = {name:name, age:age, hobby:hobby}
+        result = collection.insert_one(data)
+    except KeyError:
+        return {name:"vamsi"}
+    except ConnectionRefusedError:
+        print("Conection refused")
+    # return jsonify({"Inserted ID": str(result.inserted_id)})
     return render_template('greet.html', 
                            greeting=greeting, 
                            age_message=age_message, 
